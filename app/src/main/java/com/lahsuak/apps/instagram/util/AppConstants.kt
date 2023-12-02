4c8e8da078b1308cedff256e6377b553749cf093
Kaushal Vasava <49050597+KaushalVasava@users.noreply.github.com>
2023-12-02 22:27:32 +0530

    Update AppConstants.kt

--- app/src/main/java/com/lahsuak/apps/instagram/util/AppConstants.kt
+++ app/src/main/java/com/lahsuak/apps/instagram/util/AppConstants.kt
@@ -3,7 +3,7 @@ package com.lahsuak.apps.instagram.util
 object AppConstants {
     const val MY_USER_ID = "12340"
     const val DATE_FORMAT = "dd MMM, yy hh:mm a"
-    const val BASE_URL = "https://instagram-api-2oky.onrender.com/api/"
+    const val BASE_URL = "https://instagramrestapi.onrender.com/api/"
 
     const val SPLASH_SCREEN_TIME = 3000L
 }
